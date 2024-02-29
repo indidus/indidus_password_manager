4ae4ed8bd37e16b4b3d23c0ab96cbb646b42657b
Ashim <ashimsaha2311@gmail.com>
2024-02-29 18:55:02 +0530

    Improving biometric auth

--- lib/auth/local_auth_observer.dart
+++ lib/auth/local_auth_observer.dart
@@ -1,6 +1,61 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:local_auth/local_auth.dart';
+
+class AuthManager {
+  static final AuthManager _instance = AuthManager._();
+  static const FlutterSecureStorage _storage = FlutterSecureStorage(
+    aOptions: AndroidOptions(
+      encryptedSharedPreferences: true,
+      storageCipherAlgorithm: StorageCipherAlgorithm.AES_GCM_NoPadding,
+      keyCipherAlgorithm:
+          KeyCipherAlgorithm.RSA_ECB_OAEPwithSHA_256andMGF1Padding,
+      sharedPreferencesName: 'IndidusAuthManager',
+      resetOnError: false,
+    ),
+  );
+  factory AuthManager() {
+    return _instance;
+  }
+
+  AuthManager._();
+
+  static AuthManager get instance => _instance;
+
+  Future<bool> setAuthRequired(bool isAuthRequired) async {
+    try {
+      await _storage.write(
+        key: 'isAuthRequired',
+        value: isAuthRequired.toString(),
+      );
+      return true;
+    } catch (e) {
+      if (kDebugMode) {
+        print("error setting auth required: $e");
+      }
+      return false;
+    }
+  }
+
+  Future<bool> isAuthRequired() async {
+    try {
+      var value = await _storage.read(
+        key: 'isAuthRequired',
+      );
+      if (value == null) {
+        // Key is not set, may be first time user
+        return false;
+      }
+      return bool.parse(value, caseSensitive: false);
+    } catch (e) {
+      if (kDebugMode) {
+        print("error setting auth required: $e");
+      }
+      return true;
+    }
+  }
+}
 
 class LocalAuthObserver extends WidgetsBindingObserver {
   bool isAuthRequired = false;
@@ -14,6 +69,7 @@ class LocalAuthObserver extends WidgetsBindingObserver {
     }
     switch (state) {
       case AppLifecycleState.resumed:
+        isAuthRequired = await AuthManager.instance.isAuthRequired();
         if (!isAuthRequired) {
           return;
         }
@@ -21,8 +77,7 @@ class LocalAuthObserver extends WidgetsBindingObserver {
         bool authenticated = false;
         try {
           authenticated = await auth.authenticate(
-            localizedReason:
-                'For your data to be safe we have to verify it is you.',
+            localizedReason: 'For safety we have to verify it is you.',
             options: const AuthenticationOptions(
               stickyAuth: false,
               sensitiveTransaction: true,
@@ -30,9 +85,7 @@ class LocalAuthObserver extends WidgetsBindingObserver {
               biometricOnly: false,
             ),
           );
-          if (authenticated) {
-            isAuthRequired = false;
-          }
+          await AuthManager.instance.setAuthRequired(!authenticated);
         } catch (e) {
           if (kDebugMode) {
             print("error using biometric auth: $e");
@@ -43,7 +96,7 @@ class LocalAuthObserver extends WidgetsBindingObserver {
       case AppLifecycleState.paused:
       case AppLifecycleState.detached:
       case AppLifecycleState.hidden:
-        isAuthRequired = false;
+        isAuthRequired = await AuthManager.instance.setAuthRequired(true);
         break;
     }
   }
